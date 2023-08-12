6c4de982d9afd8e54f11573ac556d2310335aacf
Tihon <128007209+Tihon-Konstantinov@users.noreply.github.com>
2023-08-12 18:11:58 +0700

    Delete module4 directory

--- module4/lesson9/advertisements/app_advertisements/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Advertisement
-
-def index(request):
-    advertisements = Advertisement.objects.all()
-    context = {'advertisements': advertisements}
-    return render(request, 'index.html', context)
